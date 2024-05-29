bf7e9bba9498f159bdace2139d8e4e545cd41ca0
tonniegeetz <tonniegeetz@gmail.com>
2024-05-29 22:29:18 +0300

    data fetching with side effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from 'react'
+
+function App() {
+
+  const [image, setImage] = useState('')
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(
+    () => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then((res) => res.json())
+        .then((data) => setImage(data.message))
+      setIsLoaded(true)
+
+    }, []
+  )
+
+
+
+  return (
+    <div>
+      {isLoaded ? <img src={image} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  )
+}
+
+export default App
